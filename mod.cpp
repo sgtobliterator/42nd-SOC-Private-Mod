24c6fcfc824a70dc09774dc4f5d1a823fde617af
sgtobliterator <sgtobliterator@gmail.com>
2026-07-01 20:04:01 -0400

    PBOPREFIXs, mod.cpp

---
+++ mod.cpp
@@ -0,0 +1,7 @@
+name = "42nd SOC Private Mod";
+picture = "\x\42nd_SOC_PrivateMod\addons\42nd_Ui\Images\FIXED_IT_DONT_WAKE_ME_UP_AGAIN_LOL.paa";
+logo = "\x\42nd_SOC_PrivateMod\addons\42nd_Ui\Images\FIXED_IT_DONT_WAKE_ME_UP_AGAIN_LOL.paa";
+logoOver = "\x\42nd_SOC_PrivateMod\addons\42nd_Ui\Images\FIXED_IT_DONT_WAKE_ME_UP_AGAIN_LOL.paa";
+logoSmall = "\x\42nd_SOC_PrivateMod\addons\42nd_Ui\Images\FIXED_IT_DONT_WAKE_ME_UP_AGAIN_LOL.paa";
+description = "42nd SOC Private Mod";
+author = "42nd SOC";
